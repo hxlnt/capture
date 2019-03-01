6f4b9c8c6fd7084ef3aa5388ee0e3284016be905
Rachel Weil <rae@Rachels-iMac.local>
2019-02-28 23:04:21 -0600

    Add mocha

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict'
+const fs = require('fs');
 const inquirer = require("inquirer")
 
 const menu = {
@@ -8,7 +9,26 @@ const menu = {
   choices: ['Add question', 'Answer question', 'List questions', 'Add idea', 'List ideas']
 }
 
+function readFile(path){
+  fs.readFile(path, function(err, data) {
+    if (err) {
+      if (err.errno == 2) {
+        console.log(`Creating ${path}.`);
+      }
+      else { console.log(err); }
+    return err;
+    }
+    else {
+      console.log(data.toString());
+      return data;
+    }
+  }); 
+}
+
+readFile("questions.yaml");
+
 inquirer.prompt(menu).then(answers => {
   console.log(JSON.stringify(answers))
 });
 
+module.exports = readFile;
