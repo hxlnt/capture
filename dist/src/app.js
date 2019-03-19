97bcd56e8af35fd4a6a9231f47d8b8abd8cd0b49
Rachel Weil <partytimehexcellent@gmail.com>
2019-03-18 22:45:50 -0500

    Merge changes

--- dist/src/app.js
+++ dist/src/app.js
@@ -42,8 +42,8 @@ class App {
         console.log(`Question: ${yaml[entryIndex].question}`);
         console.log(`Answer: ${yaml[entryIndex].answer}`);
         console.log('+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+');
-        console.log('(q) edit question       (a) edit answer');
-        console.log('(d) delete entry            (b) go back\n');
+        console.log('(q) edit question         (a) edit answer');
+        console.log('(d) delete entry              (b) go back\n');
         inquirer.prompt(prompts.entryOptions)
             .then((answer) => {
             if (answer.entryoptions === 'a') {
@@ -55,7 +55,7 @@ class App {
             else if (answer.entryoptions === 'd') {
                 this.capDeleteQuestion(entryIndex);
             }
-            else if (answer.entryoptions === 'e') {
+            else if (answer.entryoptions === 'q') {
                 this.capEditQuestion(entryIndex);
             }
             else {
